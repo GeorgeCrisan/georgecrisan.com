c25f5d2ad6f712b14747a26545c762a92de2fb59
George Crisan <georgerdp@gmail.com>
2017-05-17 10:07:14 +0100

    ccc

---
+++ projects/ticktt/ticktt.js
@@ -0,0 +1,17 @@
+$(function() {
+
+    $('body').click(function() {
+
+        $('.container').effect('bounce', 10000);
+
+    });
+
+
+    $('body').click(function() {
+
+        $('.container').fadeIn('slow');
+
+    });
+
+
+})();
