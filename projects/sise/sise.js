cd42806070aa23d1bb24456675378e65a202bdf5
GeorgeCrisan <georgerdp@gmail.com>
2017-05-30 15:45:27 +0100

    sise

---
+++ projects/sise/sise.js
@@ -0,0 +1,18 @@
+
+
+
+jQuery(function(){
+    "use strict";
+    $.fn.bootstrapSwitch.defaults.size = 'mini';
+$.fn.bootstrapSwitch.defaults.onColor = 'success';
+$.fn.bootstrapSwitch.defaults.offColor = 'danger';
+  //function for change
+$.fn.bootstrapSwitch.defaults.onSwitchChange = function(event, state) {}
+$("[name='my-checkbox']").bootstrapSwitch();
+
+
+    console.log("jq");
+    
+   
+    
+});
